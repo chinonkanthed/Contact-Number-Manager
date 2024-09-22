d75343a500550a50563863d794342a17679b90fe
Chinon <chinonjain17@gmail.com>
2024-09-22 18:51:00 +0530

    Initial Commit

---
+++ src/main/java/project/contactapp/ContactApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package project.contactapp;
+
+/**
+ *
+ * @author gamin
+ */
+public class ContactApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
